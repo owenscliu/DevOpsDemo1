7c0b728aa5a6c6392ba0909ec11d4c81ab4b2d8e
Owen <owenliu@hkpc.org>
2021-12-23 11:38:29 +0800

    Changed to all print only

--- demo.py
+++ demo.py
@@ -1,10 +1,7 @@
 print("DEMO Program v1.0")
 print("Welcome!")
-x1=input("Proceed?[Y/N]")
-if x1=="Y":
-    print("You chose to proceed")
-else:
-    print("You quit the demo")
+print("You chose to proceed")
+print("You quit the demo")
 print("I am a change")
 print("Hong Kong Productivity Council")
 print("Written by Owen")
